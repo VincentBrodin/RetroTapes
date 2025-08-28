df48cc4124f7192ad8eb88080f89ec5835d4c002
David Browning <david.a.browning@gmail.com>
2025-08-28 15:11:30 +0200

    Update SakilaContext and add a test Actor query to Index.cshtml to verify DB connection.

--- RetroTapes/Pages/Index.cshtml.cs
+++ RetroTapes/Pages/Index.cshtml.cs
@@ -1,18 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RetroTapes.Models;
 
 namespace RetroTapes.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly SakilaContext _sakilaContext;
+    public string FirstName { get; set; } = "Default";
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, SakilaContext sakilaContext)
     {
         _logger = logger;
+        _sakilaContext = sakilaContext;
     }
 
     public void OnGet()
     {
+        Console.WriteLine("Writeline test");
+        FirstName = "Updated default";
+        FirstName = _sakilaContext.Actors.First().FirstName;
     }
 }
