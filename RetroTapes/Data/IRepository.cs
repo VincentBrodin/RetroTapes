ddf827d3753aeae0d16105f3cd6a2706235a7741
Vincent Brodin <vincent.brodin21@gmail.com>
2025-08-29 13:52:35 +0200

    added comments

--- RetroTapes/Data/IRepository.cs
+++ RetroTapes/Data/IRepository.cs
@@ -2,12 +2,16 @@ using System.Linq.Expressions;
 
 namespace RetroTapes.Data;
 
+/// <summary>
+/// Should be inherited from by all repositories in the code base.
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public interface IRepository<T>
 {
     void Add(T entity);
     void Update(T entity);
     T? Get(int id);
-    void Delete(T entity);
+    void Delete(int id);
     IEnumerable<T> All();
     IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
     void SaveChanges();
