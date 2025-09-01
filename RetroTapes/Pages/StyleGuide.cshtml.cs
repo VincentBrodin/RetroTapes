296211bd9b2639cf28272f640152adfd3b09b063
David Browning <david.a.browning@gmail.com>
2025-09-01 14:33:17 +0200

    Add color-scheme.css to shared _Layout.cshtml head section.

---
+++ RetroTapes/Pages/StyleGuide.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RetroTapes.Pages
+{
+    public class StyleGuideModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
