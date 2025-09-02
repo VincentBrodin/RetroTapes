96e804e31b52ce3bfdd2d8a399916c5f749d764e
Vincent Brodin <vincent.brodin21@gmail.com>
2025-09-02 15:37:30 +0200

    created async functions for all repositories

--- RetroTapes/Data/CustomerRepository.cs
+++ RetroTapes/Data/CustomerRepository.cs
@@ -11,9 +11,19 @@ public class CustomerRepository(SakilaContext context) : IRepository<Customer>
         context.Add(entity);
     }
 
+    public async Task AddAsync(Customer entity)
+    {
+        await context.AddAsync(entity);
+    }
+
     public IEnumerable<Customer> All()
     {
         return context.Customers.Include(c => c.Address).Include(c => c.Store);
+    }
+
+    public async Task<IEnumerable<Customer>> AllAsync()
+    {
+        return await context.Customers.Include(c => c.Address).Include(c => c.Store).ToListAsync();
     }
 
     public void Delete(int id)
@@ -22,9 +32,20 @@ public class CustomerRepository(SakilaContext context) : IRepository<Customer>
         context.Remove(entity);
     }
 
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await GetAsync(id) ?? throw new NullReferenceException($"No customer with id {id}!");
+        context.Remove(entity);
+    }
+
     public IEnumerable<Customer> Find(Expression<Func<Customer, bool>> predicate)
     {
         return context.Customers.Include(c => c.Address).Include(c => c.Store).Where(predicate);
+    }
+
+    public async Task<IEnumerable<Customer>> FindAsync(Expression<Func<Customer, bool>> predicate)
+    {
+        return await context.Customers.Include(c => c.Address).Include(c => c.Store).Where(predicate).ToListAsync();
     }
 
     public Customer? Get(int id)
@@ -32,13 +53,29 @@ public class CustomerRepository(SakilaContext context) : IRepository<Customer>
         return context.Customers.Include(c => c.Address).Include(c => c.Store).ThenInclude(s => s.Address).FirstOrDefault(c => c.CustomerId == id);
     }
 
+    public async Task<Customer?> GetAsync(int id)
+    {
+        return await context.Customers.Include(c => c.Address).Include(c => c.Store).ThenInclude(s => s.Address).FirstOrDefaultAsync(c => c.CustomerId == id);
+    }
+
     public void SaveChanges()
     {
         context.SaveChanges();
     }
 
+    public async Task SaveChangesAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+
     public void Update(Customer entity)
     {
         context.Update(entity);
+    }
+
+    public Task UpdateAsync(Customer entity)
+    {
+        context.Update(entity);
+        return Task.CompletedTask;
     }
 }
--- RetroTapes/Data/FilmRepository.cs
+++ RetroTapes/Data/FilmRepository.cs
@@ -7,38 +7,76 @@ namespace RetroTapes.Data;
 public class FilmRepository(SakilaContext context) : IRepository<Film>
 {
     public void Add(Film entity)
-{
-    context.Add(entity);
-}
+    {
+        context.Add(entity);
+    }
 
-public void Update(Film entity)
-{
-    context.Update(entity);
-}
+    public void Update(Film entity)
+    {
+        context.Update(entity);
+    }
 
-public Film? Get(int id)
-{
-    return context.Films.Include(f => f.Language).FirstOrDefault(c => c.FilmId == id);
-}
+    public Film? Get(int id)
+    {
+        return context.Films.Include(f => f.Language).FirstOrDefault(c => c.FilmId == id);
+    }
 
-public void Delete(int id)
-{
-    var entity = Get(id) ?? throw new NullReferenceException($"No film with id {id}!");
-    context.Remove(entity);
-}
+    public void Delete(int id)
+    {
+        var entity = Get(id) ?? throw new NullReferenceException($"No film with id {id}!");
+        context.Remove(entity);
+    }
 
-public IEnumerable<Film> All()
-{
-    return context.Films.Include(f => f.Language);
-}
+    public IEnumerable<Film> All()
+    {
+        return context.Films.Include(f => f.Language);
+    }
 
-public IEnumerable<Film> Find(Expression<Func<Film, bool>> predicate)
-{
-    return context.Films.Include(f => f.Language).Where(predicate);
-}
+    public IEnumerable<Film> Find(Expression<Func<Film, bool>> predicate)
+    {
+        return context.Films.Include(f => f.Language).Where(predicate);
+    }
 
-public void SaveChanges()
-{
-    context.SaveChanges();
-}
+    public void SaveChanges()
+    {
+        context.SaveChanges();
+    }
+
+    public async Task AddAsync(Film entity)
+    {
+        await context.AddAsync(entity);
+    }
+
+    public Task UpdateAsync(Film entity)
+    {
+        context.Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task<Film?> GetAsync(int id)
+    {
+        return await context.Films.Include(f => f.Language).FirstOrDefaultAsync(c => c.FilmId == id);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var entity = await GetAsync(id) ?? throw new NullReferenceException($"No film with id {id}!");
+        context.Remove(entity);
+    }
+
+    public async Task<IEnumerable<Film>> AllAsync()
+    {
+        return await context.Films.Include(f => f.Language).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Film>> FindAsync(Expression<Func<Film, bool>> predicate)
+    {
+
+        return await context.Films.Include(f => f.Language).Where(predicate).ToListAsync();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await context.SaveChangesAsync();
+    }
 }
