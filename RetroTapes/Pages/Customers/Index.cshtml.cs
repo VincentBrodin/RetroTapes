7de23174dbef632025db7f92ec3755023639fbf3
Nour Ibrahim <saeed.ibrahim1989@gmail.com>
2025-09-02 16:32:38 +0200

    adding search and filter in Customer andu unify the design

--- RetroTapes/Pages/Customers/Index.cshtml.cs
+++ RetroTapes/Pages/Customers/Index.cshtml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using RetroTapes.Data;
 using RetroTapes.Models;
+using RetroTapes.Pages.Shared;
 
 namespace RetroTapes.Pages.Customers
 {
@@ -13,11 +15,25 @@ namespace RetroTapes.Pages.Customers
             _customerRepo = customerRepo;
         }
 
-        public List<Customer> Customer { get;set; } = [];
 
-        public void  OnGet()
+        [BindProperty(SupportsGet = true)]
+        public FilterCriteriaCustomer Filter { get; set; } = new();
+
+        public List<Customer> customers { get;set; } = new List<Customer>();
+
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
+        private const int PageSize = 10;
+
+        public void OnGet(int pageIndex = 1)
         {
-            Customer = _customerRepo.All().ToList();
+            PageIndex = pageIndex;
+            var allCustomers = Filter.Run(_customerRepo.All());
+            TotalPages = (int)Math.Ceiling(allCustomers.Count() / (double)PageSize);
+            customers = allCustomers.Skip((PageIndex - 1) * PageSize).Take(PageSize).ToList();
         }
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
     }
 }
