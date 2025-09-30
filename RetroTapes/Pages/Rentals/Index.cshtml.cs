d9e7969982f451c71f61fa8cf34ab09ba856640a
David Browning <david.a.browning@gmail.com>
2025-09-30 12:13:40 +0200

    Convert RentalRepository reference to IRepository<Rental> reference in Rentals\Index.

--- RetroTapes/Pages/Rentals/Index.cshtml.cs
+++ RetroTapes/Pages/Rentals/Index.cshtml.cs
@@ -8,11 +8,11 @@ namespace RetroTapes.Pages.Rentals
 {
     public class IndexModel : PageModel
     {
-        private readonly RentalRepository _rentalRepo;
+        private readonly IRepository<Rental> _rentalRepository;
 
-        public IndexModel(RentalRepository rentalRepo)
+        public IndexModel(IRepository<Rental> rentalRepository)
         {
-            _rentalRepo = rentalRepo;
+            _rentalRepository = rentalRepository;
         }
 
         [BindProperty(SupportsGet = true)]
@@ -34,7 +34,7 @@ namespace RetroTapes.Pages.Rentals
         {
             PageIndex = pageIndex;
 
-            var query = _rentalRepo.Query();
+            var query = await _rentalRepository.AllAsync();
 
             if (!string.IsNullOrWhiteSpace(Search))
             {
@@ -56,14 +56,14 @@ namespace RetroTapes.Pages.Rentals
                 query = query.Where(r => r.ReturnDate == null);
             }
 
-            var totalCount = await query.CountAsync();
+            int totalCount = query.Count();
             TotalPages = (int)Math.Ceiling(totalCount / (double)PageSize);
 
-            Rentals = await query
+            Rentals = query
                 .OrderByDescending(r => r.RentalDate)
                 .Skip((PageIndex - 1) * PageSize)
                 .Take(PageSize)
-                .ToListAsync();
+                .ToList();
         }
     }
 
